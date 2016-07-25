d116661d54b6b82885e393ccf1a479f9047d9b0f
Larry Barrett <larry.c.barrett@gmail.com>
2016-07-25 20:35:42 +0000

    working through races; added pt -to- UTC conversion

--- app.js
+++ app.js
@@ -19,15 +19,13 @@ _.reduce(config.ids, function(p, id) {
 
 function extract(id) {
 	var filePath = config.filePath + id + buildDate();
+	var fromFile = false;
 	return fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data) {
 		if(!err) {
-console.log(' ');
-console.log(filePath + ' found');
-			transformed = transform(data);
+			fromFile = true;
+			transformed = transform(data, fromFile);
 			return load(transformed);
 		} else {
-console.log(' ');
-console.log(filePath + ' not found');
 			var options = {
 				uri: buildUrl(id),
 				headers: buildHeaders(id),
@@ -41,7 +39,7 @@ console.log(filePath + ' not found');
 console.error(err);
 						return;
 					} else {
-						transformed = transform(data);
+						transformed = transform(data, fromFile);
 						return load(transformed);
 					}
 				}); 
@@ -50,11 +48,43 @@ console.error(err);
 	});
 }
 
-function transform(data) {
-console.log(' ');
-console.log('data:');
-console.log(data);
+function transform(data, fromFile) {
+	var parsed;
+	if(fromFile) {
+		parsed = JSON.parse(data);
+	} else {
+		parsed = data;
+	}
 
+console.log(' ');
+console.log('parsed.races[0]:');
+console.log(parsed.races[0]);
+	var track = {};
+	track.name = getName(parsed.races[0].raceKey.trackId);
+	track.races = [];
+
+	parsed.races.forEach(function(race) {
+		var thisRace = {};
+		thisRace.number = race.raceKey.raceNumber;
+		thisRace.distance = race.distanceValue;
+		thisRace.formattedDistance = convertDist(race.distanceValue);
+		thisRace.surface = race.surfaceDescription;
+
+		postTimeMills = getPostTimeMills(race.postTime);
+		// time and formattedTime are UTC (EST + 4 hours)
+		thisRace.postTime = postTimeMills;
+//		formattedTime: '17:30',
+//		sexes: 'Open',
+//		ages: '2 Year Olds',
+//		type: 'Maiden Special Weight',
+//		claim: 0,
+//		purse: 75000,
+//		wagers: [
+		track.races.push(thisRace);
+	});
+console.log(' ');
+console.log('track:');
+console.log(track);
 }
 
 function load(data) {
@@ -91,3 +121,56 @@ function buildHeaders(id) {
 	return headers;
 }
 
+function getName(code) {
+	var nameMap = [];
+	nameMap['PRX'] = 'Parx';
+	return nameMap[code];
+}
+
+function convertDist(dist) {
+	var distMap = [];
+	distMap[.5625] = '4 1/2F',
+	distMap[.625] = '5F',
+	distMap[.6875] = '5 1/2F',
+	distMap[.75] = '6F',
+	distMap[.8125] = '6 1/2F',
+	distMap[.875] = '7F',
+	distMap[.9375] = '7 1/2F',
+	distMap[8] = '1M',
+	distMap[1.0625] = '1 1/16M',
+	distMap[1.070] = '1M 70Y',
+	distMap[1.125] = '1 1/8M',
+	distMap[1.25] = '1 1/4M',
+	distMap[1.375] = '1 3/8M',
+	distMap[1.5] = '1 1/2M',
+	distMap[1.625] = '1 5/8M',
+	distMap[1.75] = '1 3/4M',
+	distMap[1.875] = '1 7/8M',
+	distMap[2] = '2M'
+
+	return distMap[dist];
+}
+
+function getPostTimeMills(postTime) {
+	var raceDatePcs = buildDate().split('-');
+	var timePcs = postTime.split(' ');
+	var hourMinutePcs = timePcs[0].split(':');
+	var hour = parseInt(hourMinutePcs[0]);
+	var minute = parseInt(hourMinutePcs[1]);
+	var timeDesc = timePcs[1];
+	if(timeDesc === 'PM') {
+		if(hour < 12) {
+			hour = hour + 12;
+		}
+	}
+	var postTimeObj = new Date(
+		raceDatePcs[2], 
+		raceDatePcs[0],
+		raceDatePcs[1],
+		hour, 
+		minute, 
+		0, 
+		0
+	);
+	return postTimeObj.getTime() + 14400000;
+}
